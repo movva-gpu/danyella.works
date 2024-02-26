6457e7c847da6176370327208a5ba1aef4987581
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-26 16:17:46 +0100

    fix(back): :bug: redirection works perfectly fine now
    0.2.20 =>
    fixed redirection in projects subdomain
    
    Signed-off-by: Movva <81530181+movva-gpu@users.noreply.github.com>

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const projectRouter = express.Router();
 
 const domains = require('../conf/domains.json');
+const createError = require('http-errors');
 
 projectRouter.get('*', (req, res, next) => {
     const options = {
@@ -17,8 +21,9 @@ projectRouter.get('*', (req, res, next) => {
     if (req.hostname.split('.')[0] !== domains.projects) { next(); return; }
     if (req.url === '/' || req.url == '') { res.render('projects', { title: 'Mes projets', ...options }); return; }
     if (req.url === '/cv' || req.url === '/resume' || req.url === '/sae103') { next(); return; }
-    if (req.url === '/projects') { res.redirect('/'); }
-    res.redirect(`/projects${req.url}`);
+    if (req.url === '/projects') { res.redirect('.'); return; }
+    if (fs.existsSync(path.join(__dirname, `../www/projects${req.url}`))) { res.redirect(`/projects${req.url}`); return; }
+    next(createError(404));
 });
 
 module.exports = projectRouter;
