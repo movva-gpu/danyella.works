f49706aab2e08ee2aabb5a86305294c0d409d5d2
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-26 23:16:12 +0100

    fix(front): :bug: menu links were buggy at some moments
    0.2.22 =>
    removed js madness in layout.pug
    
    Signed-off-by: Movva <81530181+movva-gpu@users.noreply.github.com>

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -8,16 +8,6 @@ const domains = require('../conf/domains.json');
 const createError = require('http-errors');
 
 projectRouter.get('*', (req, res, next) => {
-    const options = {
-        hostname:
-            req.hostname === 'localhost' ||
-            req.hostname === '127.0.1' ?
-                'localhost:8080' :
-                req.hostname.split('.').length == 2 ?
-                    req.hostname : req.hostname.split('.')[1] + '.' + req.hostname.split('.')[2],
-        domains: domains
-    };
-
     if (req.hostname.split('.')[0] !== domains.projects) { next(); return; }
     if (req.url === '/' || req.url == '') { res.render('projects', { title: 'Mes projets', ...options }); return; }
     if (req.url === '/cv' || req.url === '/resume' || req.url.startsWith('/sae')) { next(); return; }
--- routes/router.js
+++ routes/router.js
@@ -4,16 +4,6 @@ const router = express.Router();
 const domains = require('../conf/domains.json');
 
 router.get('/', (req, res) => {
-    const options = {
-        hostname:
-            req.hostname === 'localhost' ||
-            req.hostname === '127.0.1' ?
-                'localhost:8080' :
-                req.hostname.split('.').length == 2 ?
-                    req.hostname : req.hostname.split('.')[1] + '.' + req.hostname.split('.')[2],
-        domains: domains
-    };
-
     switch (req.hostname.split('.')[0]) { // That piece of madness makes this work with any domain name, even localhost
     case domains.portfolio:
         res.render('portfolio', { title: 'Mon Portfolio', ...options });
